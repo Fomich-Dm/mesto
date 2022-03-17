c3066d02e9566f925b245dc1bf0d9792c813c573
Dmitriy Fomin <fomich.dm@yandex.ru>
2022-03-17 16:34:32 +0300

    исправление ошибок после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,29 @@ const openPopup = document.querySelector('.profile__edit-button');
 const closePopup = document.querySelector('.popup__close');
 
 const formElement = document.querySelector('.popup__info');
-const nameInput =  document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__about-me');
+const nameInput =  document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_about-me');
 const profileName = document.querySelector('.profile__name');
 const profileAddMe =  document.querySelector('.profile__about-me');
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+function popupOpen() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileAddMe.textContent;
 }
 
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
     profileName.textContent = nameInput.value;
     profileAddMe.textContent = jobInput.value;
-    togglePopup();
+    popupClose();
 }
 
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+openPopup.addEventListener('click', popupOpen);
+closePopup.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
