08ccfcd4143e0f12a7cee4d856a4cac0dcb8652c
Dmitriy Fomin <fomich.dm@yandex.ru>
2022-03-18 14:31:41 +0300

    исправления после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ const openPopup = document.querySelector('.profile__edit-button');
 const closePopup = document.querySelector('.popup__close');
 
 const formElement = document.querySelector('.popup__info');
-const nameInput =  document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_about-me');
+const nameInput =  document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_about');
 const profileName = document.querySelector('.profile__name');
 const profileAddMe =  document.querySelector('.profile__about-me');
 
